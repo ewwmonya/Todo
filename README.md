63e8f4b9a885b194b88437e2b350dfd75482e4d6
ewwmonya <savvymallen85@gmail.com>
2025-03-27 20:19:51 -0400

    Clean setup: move project to root

---
+++ README.md
@@ -0,0 +1,19 @@
+# React + Vite + Todo
+
+This Todo list ui I made since I've been working with react and JavaScript. Ive learned pretty much everything from John Smilga and if you want to too checkout his youtube then consider purchasing his course! [text](https://www.codingaddict.io/)
+
+
+## Why
+This is a classic and my fav type of project. The todo app is basiclly evey app in the world. 
+
+
+## If you found this helpful 
+Pat yourself on the back and copy and paste this. 
+
+# helpful links
+### CSS Reset
+[CSS Reset](https://www.joshwcomeau.com/css/custom-css-reset/)
+### JS Nuggets
+[JS Nuggets](https://www.youtube.com/playlist?list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ)
+
+# If I My Newborn gives me time I'll do one everyday for 365 days
---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { data } from '../../data'
+import { useState } from 'react'
+
+
+function App() {
+  const [newTodo, setNewTodo] = useState('')
+  const [todoList, setTodoList] = useState(data.map(item => ({...item, done: false})))
+  const [done, setDone] = useState(false)
+
+//    {id: 8847, todoItem: 'Get Milk'}
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    if(!newTodo) return
+    const fakeId = Date.now() % 20;
+    const submitedTodo = {id: fakeId, todoItem: newTodo, done: false}
+    // console.log(...todoList, submitedTodo);
+    setTodoList([...todoList, submitedTodo])
+    setNewTodo("")
+  }
+
+const trashCan = (id) => {
+  const updatedList = todoList.filter((todo)=> todo.id !== id);
+  setTodoList(updatedList);
+  return
+}
+
+const checkBox = (id) => {
+  const updatedList = todoList.map(todo => {
+    if(todo.id === id) {
+      return {...todo, done: !todo.done}
+    }
+    return todo
+  })
+  setTodoList(updatedList)
+}
+
+
+
+  return (
+<div className="container">
+    <h2>Todo</h2>
+
+    <form className='form' onSubmit={handleClick}>
+      <div className="form-row">
+      <label htmlFor="todoInput">
+        Type A todo
+      </label>
+      <input type="text" className='form-input' id='todoInput' value={newTodo} onChange={(e)=> setNewTodo(e.target.value)}/>
+      </div>
+    <button type="submit" className='btn'>Add Todo</button>
+    </form>
+    <h3>Todos</h3>
+    <section className='flex flex-col items-center gap-3.5' style={{fontSize: '2rem'}}>
+      {todoList.map((item)=>{
+        return (
+          <section key={item.id} className={`w-full flex justify-between border-b-indigo-400 border-b-2 pr-5`} >
+            <p className={`${item.done ? 'line-through text-green-500' : ''}`}>{item.todoItem}</p>
+            <div className='m-3.5 my-64' style={{ display: 'flex', justifyContent: 'center', gap: '30px'}}>
+              <button id='delete' type="button" style={{cursor: "pointer"}} onClick={()=> trashCan(item.id)} >🗑️</button>
+              <button id='done' type="button" style={{cursor: "pointer"}} onClick={() => {
+                checkBox(item.id)} 
+              }>
+                {item.done ? '✅' : '⬜️'}
+
+              </button>
+              </div>
+          </section> 
+        )
+      })}
+    </section>
+</div>
+  )
+}
+
+export default App
+
