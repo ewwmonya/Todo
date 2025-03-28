e23a001d641673230671d5a0a3fbc7f868f74c88
ewwmonya <savvymallen85@gmail.com>
2025-03-27 20:30:38 -0400

    new build

---
+++ data.js
@@ -0,0 +1,7 @@
+export const data = [
+    {id: 8847, todoItem: 'Get Milk'},
+    {id: 1112, todoItem: 'Get Eggs'},
+    {id: 4567, todoItem: 'Get Lentals'},
+    {id: 1234, todoItem: 'Get Salad'},
+    {id: 9876, todoItem: 'Get Rice'}
+]
